ba7d76f3eb64f2db6263e5326f8eb7f7c32ef3f1
parthpatel-ml <parthpatel.ml@gmail.com>
2025-07-20 16:33:35 +0530

    initial commit

---
+++ src/main/java/com/propyassistance/pa_ms_user_service/PaMsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propyassistance.pa_ms_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaMsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaMsUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propyassistance/pa_ms_user_service/PaMsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propyassistance.pa_ms_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaMsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
